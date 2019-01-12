d790c1d2f4b6a4a46e73e37d25275ad0d978e6ec
Edin Alibegovic <ealibegovic@capeannenterprises.com>
2019-01-12 23:46:15 +0100

    Fourth Commit

--- src/app/pages/cards/cards.component.ts
+++ src/app/pages/cards/cards.component.ts
@@ -2,14 +2,33 @@ import { Component, OnInit } from '@angular/core';
 import { CardsDataService } from '../../services/card-service/cards-data.service';
 import { MatDialog } from '@angular/material/dialog';
 import { EmployeeHistoryDialogComponent } from '../../dialogs/employee-history-dialog/employee-history-dialog.component';
+import { animate, query, stagger, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
-  styleUrls: ['./cards.component.css']
+  styleUrls: ['./cards.component.css'],
+  animations: [
+    trigger('slideInWithDelay', [
+      state('init', style({ transform: 'translateX(300%)', opacity: 0 })),
+      transition('void => start', [
+        query('mat-card', style({ transform: 'translateX(300%)', opacity: 0 })),
+        query('mat-card',
+          stagger('100ms', [
+            animate('{{time}} ease-out', style({ transform: 'translateX(0%)', opacity: 1 }))
+          ]))
+      ])
+    ]),
+  ]
 })
 export class CardsComponent implements OnInit {
   private cardsData: any;
+  public animate: any = {
+    value: 'init',
+    params: {
+      time: '750ms'
+    }
+  };
 
   constructor(private cardsDataService: CardsDataService, private dialog: MatDialog) { }
 
@@ -18,6 +37,7 @@ export class CardsComponent implements OnInit {
       this.cardsData = cardsData.body.data;
       console.log('this.cardsData ', this.cardsData);
     });
+    this.animate.value = 'start';
   }
 
   public openEmployeeHistory(employeeHistory: string): void {
